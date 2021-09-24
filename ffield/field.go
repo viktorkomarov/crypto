abff1705b52164246bca129d00c2819f6e414828
Viktor Komarov <vikomarov@ozon.ru>
2021-09-24 23:29:23 +0300

    add mul ffield

--- ffield/field.go
+++ ffield/field.go
@@ -1,1 +1,18 @@
 package ffield
+
+import "github.com/viktorkomarov/crypto/bitset"
+
+func Mul(a, b *bitset.Set) *bitset.Set {
+	result := bitset.SetFromSize(a.Size() + b.Size())
+	for i := a.Size() - 1; i >= 0; i-- {
+		if a.Nth(i) == 1 {
+			for j := b.Size() - 1; j >= 0; j-- {
+				if b.Nth(j) == 1 {
+					result.SetVal(i+j, result.Nth(i+j)^1)
+				}
+			}
+		}
+	}
+
+	return result
+}
---
+++ ffield/field_test.go
@@ -0,0 +1,50 @@
+package ffield
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/viktorkomarov/crypto/bitset"
+)
+
+func TestMul(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		a        *bitset.Set
+		b        *bitset.Set
+		expected *bitset.Set
+	}{
+		{
+			desc:     "(x^3+x+1)*(x^2+x+1)=x^5+x^4+1",
+			a:        bitset.SetFromBytes([]byte{0b11010000}),
+			b:        bitset.SetFromBytes([]byte{0b11100000}),
+			expected: bitset.SetFromBytes([]byte{0b10001100}),
+		},
+		{
+			desc:     "(x^2+x+1)*(x^3+x+1)=x^5+x^4+1",
+			b:        bitset.SetFromBytes([]byte{0b11010000}),
+			a:        bitset.SetFromBytes([]byte{0b11100000}),
+			expected: bitset.SetFromBytes([]byte{0b10001100}),
+		},
+		{
+			desc:     "(x^5+x^3+x^2+1)*1=(x^5+x^3+x^2+1)",
+			a:        bitset.SetFromBytes([]byte{0b10110100}),
+			b:        bitset.SetFromBytes([]byte{0b10000000}),
+			expected: bitset.SetFromBytes([]byte{0b10110100}),
+		},
+		{
+			desc:     "(x^2+1)*(x^2+1)=(x^4+1)",
+			a:        bitset.SetFromBytes([]byte{0b10100000}),
+			b:        bitset.SetFromBytes([]byte{0b10100000}),
+			expected: bitset.SetFromBytes([]byte{0b10001000}),
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			mul := Mul(tC.a, tC.b)
+			for i := 0; i < tC.expected.Size(); i++ {
+				require.Equalf(t, tC.expected.Nth(i), mul.Nth(i), "position %d", i)
+			}
+		})
+	}
+}
